3ef2e7fa36b7cd38c4a0de71985ff97f127f3e70
phenomenalpotato <25729550+phenomenalpotato@users.noreply.github.com>
2021-02-26 20:17:49 -0300

    UPD gamer.cpp

--- gamer.cpp
+++ gamer.cpp
@@ -9,13 +9,23 @@ int main(void) {
 
         crow::mustache::context ctx;
 
-        crow::mustache::context css;
+        // crow::mustache::context css;
 
-        auto main_page = crow::mustache::load("index.html");
+        auto main_page = crow::mustache::load("static/index.html");
 
-        auto style = crow::mustache::load("reset.css");
+        auto style = crow::mustache::default_loader("static/index.html");
 
         return main_page.render();
+
+    });
+
+        CROW_ROUTE(app, "/reset.css") ([]() {
+
+        crow::mustache::context css;
+
+        auto style = crow::mustache::load("static/reset.css");
+
+        return style.render();
 
     });
 
