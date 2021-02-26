779d43dd07779c4160178757b31cdb02ac062303
phenomenalpotato <25729550+phenomenalpotato@users.noreply.github.com>
2021-02-26 18:12:18 -0300

    UPD gamer.cpp

--- gamer.cpp
+++ gamer.cpp
@@ -3,10 +3,20 @@
 int main(void) {
 
     crow::SimpleApp app;
+    crow::mustache::set_base(".");
 
-    CROW_ROUTE(app, "/")
-    ([]() {
-        return "Hello my fellow GaMeR";
+    CROW_ROUTE(app, "/") ([]() {
+
+        crow::mustache::context ctx;
+
+        crow::mustache::context css;
+
+        auto main_page = crow::mustache::load("index.html");
+
+        auto style = crow::mustache::load("reset.css");
+
+        return main_page.render();
+
     });
 
     app.port(8080).run();
