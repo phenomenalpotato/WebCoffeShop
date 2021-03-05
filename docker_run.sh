4778627eb53f2a8c75752a73e7f4d7aeb782c74c
phenomenalpotato <25729550+phenomenalpotato@users.noreply.github.com>
2021-03-05 16:43:38 -0300

    UPD docker_run script

--- docker_run.sh
+++ docker_run.sh
@@ -13,13 +13,12 @@ docker stop webfss
 docker rm webfss
 
 # If you want to execute the Web Server
-
-# docker run -d --rm -p 8000:8080 --name webfss webfss:latest /bin/bash -c "make execute_and_compile"
+docker run -d --rm -p 8000:8080 --name webfss -e AWS_ACCESS_KEY_ID=<your_aws_access_key_id> -e AWS_SECRET_ACCESS_KEY=<your_aws_secret_access_key> webfss:latest /bin/bash -c "./gamer"
 
 # And if you want to execute without Dettach
-docker run --rm -p 8000:8080 --name webfss -e AWS_ACCESS_KEY_ID=AKIASTO337FTKDL3XJ56 -e AWS_SECRET_ACCESS_KEY=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 webfss:latest /bin/bash -c "./gamer"
+# docker run --rm -p 8000:8080 --name webfss -e AWS_ACCESS_KEY_ID=<aws_access_key_id> -e AWS_SECRET_ACCESS_KEY=<aws_secret_access_key> webfss:latest /bin/bash -c "./gamer"
 
 # docker run --rm -p 8000:8080 --name webfss webfss:latest /bin/bash -c "make execute_and_compile"
 
-# If you want to execute the S3-Upload-Object/s3-upload.cpp
+# If you want to execute the S3-Upload-Object/s3-upload.cpp-- NOT WORKING
 #docker run --name webfss webfss:latest /bin/bash -c "make execute_s3_sample"
